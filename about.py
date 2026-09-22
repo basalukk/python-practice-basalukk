2d7a0633c807190c4f3f5bf49c3cc0cae074845e
Illia Basalukk <himry12@gmail.com>
2026-09-22 12:26:03 +0300

    Add personal information script

---
+++ about.py
@@ -0,0 +1,7 @@
+print("Illia Basalukk, IT-31")
+print("Name: Illia")
+print("Surname: Basalukk")
+print("Group: IT-31")
+print("Birth date: 11.07.2009")
+print("Birth city: Lutsk")
+print("Hobbies: drawing, gym, gaming")
